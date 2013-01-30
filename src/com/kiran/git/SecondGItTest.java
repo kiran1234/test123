7ab58d57367acd98841d93b17b94afc2afcae7b6
kiran.babu <kiran.babu@DXT47V1PC.fei.local>
2013-01-30 09:14:36 -0500

    test adding index

---
+++ src/com/kiran/git/SecondGItTest.java
@@ -0,0 +1,16 @@
+package com.kiran.git;
+
+public class SecondGItTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("TEsting git secondGItTEst");
+		
+		System.out.println("Index after");
+	}
+
+}
